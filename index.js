8b34a8fb5b5cb62a9c460be33e01b4162100818e
Joshua Miles <joshua.m@hackerusa.com>
2021-03-03 16:46:01 -0600

    -

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function newImage(url, left, bottom){
     return image
 }
 
+newImage('assets/green-character.gif', 100, 250)
+newImage('assets/tree.png', 200, 450)
+newImage('assets/pillar.png', 350, 250)
+newImage('assets/pine-tree.png', 450, 350)
+newImage('assets/crate.png', 150, 350)
+newImage('assets/well.png', 500, 575)
+
+
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
     item.addEventListener('click', () => {
@@ -18,6 +26,10 @@ function newItem(url, left, bottom){
     })
     return item
 }
+
+newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -36,15 +48,4 @@ function newInventory(){
     return inventory
 }
 
-
 const inventory = newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
